97500f6d9b6504aec4d3d573832ce2e02c0d3a63
Cernousekkk <pavlikcerny25@gmail.com>
2026-04-28 23:40:12 +0200

    pridani projektu

---
+++ BudgetTracker/FinanceService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetTracker
+{
+    public class FinanceService
+    {
+        public void AddTransaction(Transactions t)
+        {
+            
+        }
+
+        public decimal GetCurrentBalance(int userId)
+        {
+            return 0;
+        }
+    }
+}
---
+++ BudgetTracker/Users.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetTracker
+{
+    public class Users
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+
+        public Users(string firstName, string lastName, string password)
+        {
+            firstName = FirstName;
+            lastName = LastName;
+            password = Password;
+        }
+
+    }
+}
