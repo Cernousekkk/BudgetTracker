553275ddf51fbe4de3abb81d439c1f339a3dc46c
Cernousekkk <pavlikcerny25@gmail.com>
2026-05-05 21:28:07 +0200

    Add DbContext, Tables, Database connection

--- BudgetTracker/FinanceService.cs
+++ BudgetTracker/FinanceService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BudgetTracker.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,7 @@ namespace BudgetTracker
 {
     public class FinanceService
     {
-        public void AddTransaction(Transactions t)
+        public void AddTransaction(Transaction t)
         {
             
         }
--- BudgetTracker/obj/Debug/net8.0-windows/BudgetTracker.AssemblyInfo.cs
+++ BudgetTracker/obj/Debug/net8.0-windows/BudgetTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BudgetTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97500f6d9b6504aec4d3d573832ce2e02c0d3a63")]
 [assembly: System.Reflection.AssemblyProductAttribute("BudgetTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BudgetTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
